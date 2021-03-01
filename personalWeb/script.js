9b410a6c76e23194c1a0f26261b0dee68dfa12bc
ranggahalimp <79045941+ranggahalimp@users.noreply.github.com>
2021-03-01 15:35:54 +0700

    Add files via upload

---
+++ personalWeb/script.js
@@ -0,0 +1,23 @@
+const txtElement = ["Web deveopment", "Desain"];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = "";
+let words = "";
+
+(function ngtik() {
+    if (count == txtElement.length) {
+        count = 0;
+    }
+
+    currentTxt = txtElement[count];
+
+    words = currentTxt.slice(0, ++txtIndex);
+    document.querySelector(".efek").textContent = words;
+
+    if (words.length == currentTxt.length) {
+        count++;
+        txtIndex = 0;
+    }
+
+    setTimeout(ngtik, 700);
+})();
